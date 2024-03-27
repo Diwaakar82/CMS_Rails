5566a845d6d01d8526ce4b2cba6abe42f3429f92
Diwaakar82 <diwaakar@mobicip.com>
2024-03-27 16:03:40 +0530

    Added edit profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(email: params[:user][:email].downcase)
         if @user && @user.authenticate(params[:user][:password])
-            puts "^^^^^", @user.id
-            # reset_session
+            reset_session
             session[:current_user_id] = @user.id
             redirect_to authenticated_root_path, notice: "Logged in"
         else
@@ -15,8 +14,27 @@ class SessionsController < ApplicationController
         end
     end
     
+    def edit
+        @user = current_user
+    end
+
+    def update
+        @user = current_user
+
+        if @user.update(user_params)
+            redirect_to authenticated_root_url
+        else
+            render 'edit'
+        end
+    end
+
     def destroy
         reset_session
         redirect_to login_url
+    end
+
+private
+    def user_params
+        params.require(:user).permit(:email, :password)
     end
 end
